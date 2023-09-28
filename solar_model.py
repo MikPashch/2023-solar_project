3758cfc7cc112026e2e4dbb27ef8bddc6f5f4655
Mike Pashchenko <99202102+MikPashch@users.noreply.github.com>
2023-09-28 13:15:05 +0200

    Update solar_model.py
    translated

--- solar_model.py
+++ solar_model.py
@@ -2,22 +2,22 @@
 # license: GPLv3
 
 gravitational_constant = 6.67408E-11
-"""Гравитационная постоянная Ньютона G"""
+"""Newton's gravitational constant G"""
 
 
 def calculate_force(body, space_objects):
-    """Вычисляет силу, действующую на тело.
+    """Calculates the force acting on a body.
 
-    Параметры:
+    Parameters:
 
-    **body** — тело, для которого нужно вычислить дейстующую силу.
-    **space_objects** — список объектов, которые воздействуют на тело.
+    **body** - the body for which to calculate the acting force.
+    **space_objects** - a list of objects that exert an influence on the body.
     """
 
     body.Fx = body.Fy = 0
     for obj in space_objects:
         if body == obj:
-            continue  # тело не действует гравитационной силой на само себя!
+            continue  # A body doesn't exert gravitational force on itself!
         r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
         f = gravitational_constant * body.m * obj.m / r ** 2
         body.Fx += -f * (body.x - obj.x) / r
@@ -25,11 +25,11 @@ def calculate_force(body, space_objects):
 
 
 def move_space_object(body, dt):
-    """Перемещает тело в соответствии с действующей на него силой.
+    """Moves a body according to the force acting on it.
 
-    Параметры:
+    Parameters:
 
-    **body** — тело, которое нужно переместить.
+    **body** - the body to be moved.
     """
 
     ax = body.Fx / body.m
@@ -41,12 +41,12 @@ def move_space_object(body, dt):
 
 
 def recalculate_space_objects_positions(space_objects, dt):
-    """Пересчитывает координаты объектов.
+    """Recalculates the coordinates of objects.
 
-    Параметры:
+    Parameters:
 
-    **space_objects** — список оьъектов, для которых нужно пересчитать координаты.
-    **dt** — шаг по времени
+    **space_objects** - a list of objects for which to recalculate coordinates.
+    **dt** - time step
     """
 
     for body in space_objects:
